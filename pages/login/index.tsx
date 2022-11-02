2bd2cd1a10794f8312d1eb6c79f8daedbddad144
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-11-02 17:11:00 -0500

    Login Mensajes

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -90,37 +90,41 @@ export default function Login() {
   } = formik;
 
   return (
-    <div className='auth'>
-      <form onSubmit={handleSubmit}>
-        <h1>Login</h1>
-        <div>
-          <label>Email</label>
-          <input
-            name='email'
-            type='email'
-            value={values.email}
-            onChange={handleChange}
-          />
-          {errors.email && touched.email && <div>{errors.email}</div>}
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            name='password'
-            type='password'
-            value={values.password}
-            onChange={handleChange}
-          />
-          {errors.password && touched.password && <div>{errors.password}</div>}
-        </div>
-        <div>
-          <button type='submit'>Enviar</button>
-        </div>
-        <div>
-          <Link href='/register'>Register</Link>
-        </div>
-      </form>
+    <>
+      <div className='auth'>
+        <form onSubmit={handleSubmit}>
+          <h1>Login</h1>
+          <div>
+            <label>Email</label>
+            <input
+              name='email'
+              type='email'
+              value={values.email}
+              onChange={handleChange}
+            />
+            {errors.email && touched.email && <div>{errors.email}</div>}
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              name='password'
+              type='password'
+              value={values.password}
+              onChange={handleChange}
+            />
+            {errors.password && touched.password && (
+              <div>{errors.password}</div>
+            )}
+          </div>
+          <div>
+            <button type='submit'>Enviar</button>
+          </div>
+          <div>
+            <Link href='/register'>Register</Link>
+          </div>
+        </form>
+      </div>
       <Notify />
-    </div>
+    </>
   );
 }
