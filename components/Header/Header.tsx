b69b17d4d6fa4912aa43c86806248703fa883d05
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-11-02 21:50:50 -0500

    Logout

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,37 @@
 /** @format */
 
 import React from 'react';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
+import { Notify } from '../../components/Notify';
 
 export const Header = () => {
+  const router = useRouter();
+
+  const successToast = (message: string) => {
+    toast.success(message, {
+      position: 'top-center',
+      autoClose: 2500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+      progress: undefined,
+      theme: 'light',
+    });
+  };
+
+  const logout = () => {
+    successToast('You have successfully logout!');
+    sessionStorage.removeItem('token');
+    setTimeout(() => router.push('/login'), 3700);
+  };
+
   return (
     <header>
+      <Notify />
       <span>Meow</span>
-      <div>X</div>
+      <div onClick={logout}>X</div>
     </header>
   );
 };
